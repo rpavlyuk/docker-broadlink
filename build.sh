8bbed2ddb6b842fef7c523ed1f48b4b26a43fefc
Roman Pavlyuk <roman.pavlyuk@gmail.com>
2020-05-04 13:01:00 +0300

    Enhanced build script

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ VERSION="1.0"
 CONTAINER_NAME="docker.io/rpavlyuk/c7-broadlink"
 
 # Run svarog to build the RPMs
-svarog
+svarog || exit 1
 
 # collect packages
-rm -rf .rpms && mkdir -p .rpms
-find ./.repo -type f -name "*.rpm" -exec cp {} ./.rpms \;
+rm -rf .rpms && mkdir -p .rpms || exit 1
+find ./.repo -type f -name "*.rpm" -exec cp {} ./.rpms \; || exit 1
 
 # Build docker container
 docker build --rm -t $CONTAINER_NAME .
